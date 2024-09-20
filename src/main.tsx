3dadd33012058decf678c71be0a7158e1c5ffbf0
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-19 22:02:32 -0400

    improve toolbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import './assets/main.css'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import "./assets/main.css"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />)
