4dcf30a81ba540210b8bad0faf07ce58f5644447
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-26 10:00:03 -0400

    add 梵天

--- src/MMDScene.tsx
+++ src/MMDScene.tsx
@@ -26,7 +26,7 @@ import ammoPhysics from "./ammo/ammo.wasm"
 import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material"
 
 const defaultModel = "深空之眼-托特"
-const availableModels = ["深空之眼-托特", "深空之眼-托特2", "深空之眼-芭丝特", "鸣潮-吟霖", "原神-荧"]
+const availableModels = ["深空之眼-托特", "深空之眼-大梵天", "深空之眼-芭丝特", "鸣潮-吟霖", "原神-荧"]
 
 function MMDScene({
   pose,
