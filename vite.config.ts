d572d17f64eadb10451e0ecf3073bd048ddf3479
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-27 11:43:46 -0400

    add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["babylon-mmd", "babylon-mmd/esm/Loader/pmxLoader"],
+    exclude: ["babylon-mmd"],
   },
   server: {
     headers: {
