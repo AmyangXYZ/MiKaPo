6fd5e5d14f743125edc61725b220fbfe0d9be103
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-27 11:58:24 -0400

    remove gtag

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
+import { Analytics } from "@vercel/analytics/react"
 
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+)
