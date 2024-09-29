0cdf245b2067c6e97f91a6fe6a4f67a57bef76c5
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-28 20:40:02 -0400

    fix pwa workbox limit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 5 MB
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["babylon-mmd"],
   },
