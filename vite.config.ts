e5b33565a5079df6ad67da608270be710e993a78
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-28 20:29:28 -0400

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   optimizeDeps: {
     exclude: ["babylon-mmd"],
   },
