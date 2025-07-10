48af8e7894f6ce2417e2ab062637fc4ec5e67663
AmyangXYZ <amyang.xyz@gmail.com>
2025-07-09 20:57:23 -0400

    add pwa back

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // Custom plugin to handle the problematic wasm-bindgen-rayon file
 const wasmBindgenPlugin = () => {
@@ -20,7 +21,35 @@ const wasmBindgenPlugin = () => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasmBindgenPlugin()],
+  plugins: [
+    react(),
+    wasmBindgenPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Mikapo",
+        short_name: "Mikapo",
+        description: "AI Pose Picker For MMD",
+        theme_color: "#000000",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["babylon-mmd", "pose_solver"],
   },
